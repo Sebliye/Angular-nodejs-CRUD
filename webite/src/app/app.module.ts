7a9350179fe4b085b226063b95f9764694316eb3
Seble Aygoda <saygoda@mum.edu>
2021-03-21 14:37:00 -0500

    structure

--- webite/src/app/app.module.ts
+++ webite/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ListComponent } from './list.component';
+import { CreateComponent } from './create.component';
+import { EditComponent } from './edit.component';
+import {Routes,RouterModule} from '@angular/router';
+import { FooterComponent } from './footer.component';
+import { HeaderComponent } from './header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CreateComponent,
+    EditComponent,
+    FooterComponent,
+    HeaderComponent
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
